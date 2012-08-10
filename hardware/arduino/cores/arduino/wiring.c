43253494c21e74ddc1f229bf4c9de73206a0ff30
Weihong Guan <weihong.guan@gmail.com>
2012-08-10 15:10:58 +0800

    delete comment for deleted code

--- hardware/arduino/cores/arduino/wiring.c
+++ hardware/arduino/cores/arduino/wiring.c
@@ -26,10 +26,6 @@
 
 #include "wiring_private.h"
 
-// the prescaler is set so that timer0 ticks every 64 clock cycles, and the
-// the overflow handler is called every 256 ticks.
-// #define MICROSECONDS_PER_TIMER0_OVERFLOW (clockCyclesToMicroseconds(64 * 256))
-
 #define TIMER0_PRESCALE	64
 
 const uint16_t US_PER_TIMER0_OVERFLOW = TIMER0_PRESCALE * 256 / clockCyclesPerMicrosecond(); // typical value: 1024
@@ -37,18 +33,7 @@ const uint8_t US_PER_TIMER0_COUNT = TIMER0_PRESCALE / clockCyclesPerMicrosecond(
 // W.H. Guan: Do this long division just once. US means us unit, microsecond, 10 ^ (-6) second.
 
 
-// the whole number of milliseconds per timer0 overflow
-//#define MILLIS_INC (MICROSECONDS_PER_TIMER0_OVERFLOW / 1000)
-
-// the fractional number of milliseconds per timer0 overflow. we shift right
-// by three to fit these numbers into a byte. (for the clock speeds we care
-// about - 8 and 16 MHz - this doesn't lose precision.)
-//#define FRACT_INC ((MICROSECONDS_PER_TIMER0_OVERFLOW % 1000) >> 3)
-//#define FRACT_MAX (1000 >> 3)
-
 volatile unsigned long timer0_overflow_count = 0;
-//volatile unsigned long timer0_millis = 0;
-//static unsigned char timer0_fract = 0;
 
 #if defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
 SIGNAL(TIM0_OVF_vect)
@@ -56,21 +41,6 @@ SIGNAL(TIM0_OVF_vect)
 SIGNAL(TIMER0_OVF_vect)
 #endif
 {
-	// copy these to local variables so they can be stored in registers
-	// (volatile variables must be read from memory on every access)
-	//	unsigned long m = timer0_millis;
-	//	unsigned char f = timer0_fract;
-	//
-	//	m += MILLIS_INC;
-	//	f += FRACT_INC;
-	//	if (f >= FRACT_MAX) {
-	//		f -= FRACT_MAX;
-	//		m += 1;
-	//	}
-	//
-	//	timer0_fract = f;
-	//	timer0_millis = m;
-	// W.H. Guan:
 	timer0_overflow_count++;
 }
 
