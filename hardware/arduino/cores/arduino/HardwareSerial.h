b4f45b503d14dec863610d8ce2ddd93667e721d7
Weihong Guan <weihong.guan@gmail.com>
2012-08-28 00:34:46 +0800

    modify HardwareSerial class to pure C++ class

--- hardware/arduino/cores/arduino/HardwareSerial.h
+++ hardware/arduino/cores/arduino/HardwareSerial.h
@@ -17,51 +17,64 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
  Modified 28 September 2010 by Mark Sproul
+
+ Modified August 28 2012 by Weihong Guan (aGuegu) to convert to a pure c++ class
  */
 
 #ifndef HardwareSerial_h
 #define HardwareSerial_h
 
 #include <inttypes.h>
-
+#include <avr/interrupt.h>
 #include "Stream.h"
 
-struct ring_buffer;
+struct ring_buff
+{
+	uint8_t *buffer;
+	volatile uint8_t index_write;	// to receive the incoming byte
+	volatile uint8_t index_read;	// to send byte
+};
 
 class HardwareSerial: public Stream
 {
-private:
-	ring_buffer *_rx_buffer;
-	ring_buffer *_tx_buffer;
-	volatile uint8_t *_ubrrh;
-	volatile uint8_t *_ubrrl;
-	volatile uint8_t *_ucsra;
-	volatile uint8_t *_ucsrb;
-	volatile uint8_t *_udr;
-	uint8_t _rxen;
-	uint8_t _txen;
-	uint8_t _rxcie;
-	uint8_t _udrie;
-	uint8_t _u2x;
 public:
-	HardwareSerial(ring_buffer *rx_buffer, ring_buffer *tx_buffer,
-			volatile uint8_t *ubrrh, volatile uint8_t *ubrrl,
+	HardwareSerial(volatile uint8_t *ubrrh, volatile uint8_t *ubrrl,
 			volatile uint8_t *ucsra, volatile uint8_t *ucsrb,
 			volatile uint8_t *udr, uint8_t rxen, uint8_t txen, uint8_t rxcie,
-			uint8_t udrie, uint8_t u2x);
+			uint8_t udrie, uint8_t u2x, uint8_t buff_size);
 	virtual ~HardwareSerial()
 	{
 	}
 	;
-	void begin(unsigned long);
+	void begin(unsigned long baudrate);
 	void end();
 	virtual int available(void);
 	virtual int peek(void);
 	virtual int read(void);
 	virtual void flush(void);
 	virtual size_t write(uint8_t);
-	using Print::write; // pull in write(str) and write(buf, size) from Print
+	//using Print::write; // pull in write(str) and write(buf, size) from Print
+	void transmit();
+	void receive();
+
 	operator bool();
+
+private:
+	volatile uint8_t * const _ubrrh;
+	volatile uint8_t * const _ubrrl;
+	volatile uint8_t * const _ucsra;
+	volatile uint8_t * const _ucsrb;
+	volatile uint8_t * const _udr;
+	const uint8_t _rxen;
+	const uint8_t _txen;
+	const uint8_t _rxcie;
+	const uint8_t _udrie;
+	const uint8_t _u2x;
+	const uint8_t _buff_size;
+
+	ring_buff _tx_buff;
+	ring_buff _rx_buff;
+
 };
 
 #if defined(UBRRH) || defined(UBRR0H)
